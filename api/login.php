cfe8f0a6ed221924f63a9c8127b29a62b809ec36
Ubuntu <ubuntu@ip-10-199-64-24.us-west-1.compute.internal>
2014-05-29 07:04:41 +0000

    more stuff

--- api/login.php
+++ api/login.php
@@ -5,7 +5,7 @@ require("config.inc.php");
 $phone = mysqli_real_escape_string($db, $_POST['phone']);
 $password = mysqli_real_escape_string($db, $_POST['password']);
 
-$ret = mysqli_fetch_assoc(mysqli_query($db, "SELECT * FROM `users` WHERE `phone` = '$phone' AND `password` = '$password'"));
+$ret = mysqli_fetch_assoc(mysqli_query($db, "SELECT `id`, `phone` FROM `users` WHERE `phone` = '$phone' AND `password` = '$password'"));
 
 if (is_array($ret)) {
   header("Content-Type: text/json");
